6b5798a9410584b29fec0083b4cd3a1db587c097
ChiNguyen <dacchi6761@gmail.com>
2020-04-14 09:47:06 +0700

    Update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,13 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:admin'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ],
+        [
+            'name.required'=>'Tên không được để trống',
+            'name.unique'=>'Email đã tồn tại',
+            'password.confirmed'=>'Mật khẩu không trùng khớp',
+            'password.min'=>'Mật khẩu it nhất 8 kí tự',
+
         ]);
     }
 
