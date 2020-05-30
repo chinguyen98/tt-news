3ee6185dd8150a023c5e80e98fed9330a3099f1e
chinguyen98 <dacchi6761@gmail.com>
2020-05-30 22:59:21 +0700

    verify email when register

--- app/Admin.php
+++ app/Admin.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class Admin extends Authenticatable
+class Admin extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
